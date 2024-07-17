fdefc27c40f83bb32e82289931ad777c950ed6e7
pydevd <pydevd@proton.me>
2024-07-17 16:48:44 +0000

    Autopairs, git status

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,13 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+		require("neo-tree").setup({
+			default_component_configs = {
+				name = {
+					use_git_status_colors = true,
+				},
+			},
+		})
 		vim.keymap.set("n", "<M-e>", ":Neotree filesystem reveal left<CR>", {})
 	end,
 }
